940735af04f84cfe9426263d1627093b4e6c0b91
grandwizard28 <vibhupandey28@gmail.com>
2021-11-27 22:21:34 +0530

    Fix

--- terraform/scripts/cassandra_user_data.sh
+++ terraform/scripts/cassandra_user_data.sh
@@ -3,22 +3,39 @@
 BUCKET_NAME=${T_BUCKET_NAME}
 BUCKET_KEY=${T_BUCKET_KEY}
 
-apt update
-apt install openjdk-8-jdk awscli apt-transport-https -y
+function start_cassandra(){
+    systemctl enable cassandra
+    systemctl start cassandra
+}
 
-sh -c 'echo "deb http://www.apache.org/dist/cassandra/debian 40x main" > /etc/apt/sources.list.d/cassandra.list'
-wget -q -O - https://www.apache.org/dist/cassandra/KEYS | sudo apt-key add -
+function download_config(){
+    aws s3 cp s3://$BUCKET_NAME/$BUCKET_KEY/ /etc/cassandra --recursive
+}
 
-apt update
-apt install cassandra -y
-systemctl enable cassandra
-systemctl stop cassandra
-sudo rm -rf /var/lib/cassandra/*
-sleep 1m
+function stop_cassandra(){
+    systemctl stop cassandra
+    sudo rm -rf /var/lib/cassandra/*
+}
 
-aws s3 cp s3://$BUCKET_NAME/$BUCKET_KEY/ /etc/cassandra --recursive
+function install_cassandra(){
+    sh -c 'echo "deb http://www.apache.org/dist/cassandra/debian 40x main" > /etc/apt/sources.list.d/cassandra.list'
+    wget -q -O - https://www.apache.org/dist/cassandra/KEYS | sudo apt-key add -
+    apt update
+    apt install cassandra -y
+}
 
-systemctl start cassandra
+function install_dependencies(){
+    apt update
+    apt install openjdk-8-jdk awscli apt-transport-https -y
+}
 
+function main(){
+    install_dependencies
+    install_cassandra
+    stop_cassandra
+    sleep 1m
+    download_config
+    start_cassandra
+}
 
-
+main "@"
